3f4901359b42952c6fb8e1e71c04ad3bd4cd331c
Alex Bianchi <alexbbianchi@gmail.com>
2022-10-23 20:07:31 -0300

    Adicionado loading

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,23 +1,36 @@
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
+import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 
 import { Course } from '../model/course';
+import { catchError, Observable, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-
-  courses: Course[] = [
-    { _id: '1', name: "Angular", category: 'front-end'},
-    { _id: '1', name: "React", category: 'front-end'},
-  ];
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor() {  }
-
-  ngOnInit(): void {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
+    this.courses$ = this.coursesService.list().pipe(
+      catchError((error) => {
+        this.onError("Erro ao carregar cursos.");
+        return of([]);
+      })
+    );
   }
 
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    })
+  }
+  ngOnInit(): void {}
 }
--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+
 @NgModule({
   exports: [
-  MatTableModule,
-  MatCardModule,
-  MatToolbarModule
-  ]
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule
+  ],
 })
-export class AppMaterialModule { }
+export class AppMaterialModule {}
