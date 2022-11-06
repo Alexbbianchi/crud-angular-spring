e75af6e99d23ee88e5b75cdbed7e37cceaa4c412
Alex Bianchi <alexbbianchi@gmail.com>
2022-11-06 17:48:23 -0300

    Iniciado métodos de update

--- crud-spring/src/main/java/com/alex/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/alex/crudspring/controller/CourseController.java
@@ -4,8 +4,16 @@ import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alex.crudspring.model.Course;
@@ -21,8 +29,21 @@ public class CourseController {
     private final CourseRepository courseRepository;
     
     @GetMapping
-    public List<Course> list() {
+    public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+   
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+            .map(record -> ResponseEntity.ok().body(record))
+            .orElse(ResponseEntity.notFound().build());
+    }
+    
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+       return courseRepository.save(course);
     }
     
     @Bean
--- crud-spring/src/main/java/com/alex/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/alex/crudspring/model/Course.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
@@ -16,6 +18,7 @@ public class Course {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)
