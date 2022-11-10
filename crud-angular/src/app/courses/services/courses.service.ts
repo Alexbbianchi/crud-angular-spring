3264e64a533992afe2aba885c935fc5a08cfed9b
Alex Bianchi <alexbbianchi@gmail.com>
2022-11-10 20:34:52 -0300

    Criado métodos para update

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -29,6 +29,17 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>) {
+    if (record._id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
   }
 }
